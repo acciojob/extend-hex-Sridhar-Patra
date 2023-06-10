3dad9efdc58ded58aa9766ad882fbffebda10daf
Sridhar Patra <73095300+Sridhar-Patra@users.noreply.github.com>
2023-06-10 12:51:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const extendHex = (shortHex) => {
   // write your code here
+	 if short_hex[0] == '#':
+        short_hex = short_hex[1:]
+
+    // # Extend each color component
+    full_hex = ''.join([c * 2 for c in short_hex])
+
+    // # Add the '#' character and return the full hex code
+    return '#' + full_hex
 };
 
 // Do not change the code below.
